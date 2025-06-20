5ef0f61223d45b728e2180ca7824e1f6272fa2fa
camtariel <camille.tariel@fft.fr>
2025-06-20 11:21:53 +0200

    Update PetClinicApplication.java
    Signed-off-by: camtariel <camille.tariel@fft.fr>

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -35,3 +35,57 @@ public class PetClinicApplication {
 	}
 
 }
+
+public class ProblematicCode {
+
+    // Unused variable
+    private int unusedVariable;
+
+    // Method with cognitive complexity and a long parameter list
+    public void complexMethod(int param1, int param2, int param3, int param4, int param5) {
+        // High cognitive complexity due to nested loops and conditions
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i > j) {
+                    if (j < i) {
+                        if (param1 > param2) {
+                            System.out.println("Complex logic here");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Empty catch block
+    public void riskyOperation() {
+        try {
+            // Risky operation that may throw an exception
+            int result = 10 / 0;
+        } catch (Exception e) {
+            // Empty catch block
+        }
+    }
+
+    // Duplicate code block
+    public void duplicateCodeExample() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+
+    public void anotherDuplicateCodeExample() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+
+    // Hardcoded password
+    private String password = "password123";
+
+    // Method with too many lines
+    public void longMethod() {
+        // Imagine this method has more than 100 lines of code
+        // ...
+    }
+}
